c2344a7616bf62631c8073733adfd8b676f6cf59
robi <robin.sarfati@realconsultingdata.com>
2023-03-23 19:35:29 +0100

    :sparkles: rust server running

---
+++ server/src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use] extern crate rocket;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Front end can query!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().attach(CORS).mount("/", routes![index])
+}
